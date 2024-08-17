1fbe60a80ab66f2df8c7556115ec2309667c2093
Mateja <matejamarsenic@gmail.com>
2024-08-17 15:37:44 +0200

    Item correction grid

--- scenes/grid_drawing.gd
+++ scenes/grid_drawing.gd
@@ -4,15 +4,26 @@ extends Node2D
 
 
 var CELL_COUNT = 32
+var offset = get_viewport_rect().size.y / CELL_COUNT; 
 
-func _draw():
+func get_offset():
+	return get_viewport_rect().size.y / CELL_COUNT
 	
+func _draw():
+	redraw_elements()
 	var half_width = get_viewport_rect().size.y / 2
 	var half_height = get_viewport_rect().size.y / 2
-	var offset = half_height*2/CELL_COUNT;
+	offset = get_offset()
 
 	
 	for i in range(CELL_COUNT+1):
 			draw_line(Vector2(-half_width, -half_height+i*offset),Vector2(half_width, -half_height+i*offset ),Color.GRAY)
 	for j in range(CELL_COUNT+1):
 			draw_line(Vector2(-half_width+j*offset, -half_height),Vector2(-half_width+j*offset, half_height ),Color.GRAY)
+
+	
+func redraw_elements():
+	for N : Node2D in self.get_children():
+		N.scale = Vector2(4 * get_offset() / 256,4 * get_offset() / 256)
+		print(N.scale)
+		
